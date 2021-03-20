0012b4aa66117cd2f1a37a33a21bce8e9346758e
Ahmad Arsim <79629827+ahmadarsim@users.noreply.github.com>
2021-03-21 00:50:39 +0500

    Matrix.r
    practice file for matrix

---
+++ Matrix.r
@@ -0,0 +1,18 @@
+m <- matrix(11 :6, nrow =2 , ncol = 3)
+print(m)
+dim(m)
+dim (m) <- c(3,2)
+attributes(m)
+y<- 1:9
+dim(y) <- c(3,3)
+
+m <- matrix(1:25, nrow = 5,ncol = 5)
+print(m)
+dim(m)
+attributes(m)
+a<- 1:49
+print(a)
+dim(a) <-c(7,7)
+
+print(a)
+attributes(a)
